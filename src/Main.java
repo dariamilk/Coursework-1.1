dad936ab1b224dd1da40a5dfc800a9ef34fc402f
dariamilk <daryachulpanova@gmail.com>
2023-03-19 13:47:24 +0300

    Coursework, examination card 6

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    public static double toSquare (double a) {
+        return a*a;
+    }
     public static void main(String[] args) {
+        System.out.println(toSquare(6.46));
     }
 }
